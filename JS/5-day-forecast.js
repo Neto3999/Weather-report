2ea804896749382b27c4b31e74fadeed6098e367
Neto3999 <netowashere@gmail.com>
2023-07-27 13:13:00 -0700

    Added 5 days
    shows the ability to have 5 days be shown if i decide to go that route

--- JS/5-day-forecast.js
+++ JS/5-day-forecast.js
@@ -4,13 +4,13 @@ const key = '1ulTMdI2OkIdrSZax6S7qB56wOCxKbp6';
  //Weather Forecast//
 const getWeather = async (id) => {
 
-    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
     const data = await response.json();
 
-    return data[0];
+    return data;
 };
 
 // Search city and city key//
--- JS/UI-Display.js
+++ JS/UI-Display.js
@@ -1,4 +1,4 @@
-const weatherSummary = document.getElementById("weather_summary");
+const daySummary = document.getElementById("day_forecast");
 const weatherPic = document.querySelector("body");
 const icon = document.getElementById("icon");
 
@@ -8,16 +8,69 @@ const apiData = (data) => {
 
     const { cityDetails, weather } = data;
 
+
 //Weather icon
-const iconSrc = `SamplePng/${weather.WeatherIcon}.png`;
-icon.setAttribute('src', iconSrc);
+const iconSrc1 = `SamplePng/${weather.DailyForecasts[0].Day.Icon}.png`;
+const iconSrc2 = `SamplePng/${weather.DailyForecasts[1].Day.Icon}.png`;
+const iconSrc3 = `SamplePng/${weather.DailyForecasts[2].Day.Icon}.png`;
+const iconSrc4 = `SamplePng/${weather.DailyForecasts[3].Day.Icon}.png`;
+const iconSrc5 = `SamplePng/${weather.DailyForecasts[4].Day.Icon}.png`;
+
+
+//Formatting the date
+function formatDate(dateString) {
+    const dateObject = new Date(dateString);
+    const month = dateObject.getMonth() + 1;
+    const date = dateObject.getDate();
+    return `${month.toString().padStart(2, "0")}-${date.toString().padStart(2, "0")}`;
+};
+
+//Getting the date
+const formattedDate1 = formatDate(weather.DailyForecasts[0].Date);
+const formattedDate2 = formatDate(weather.DailyForecasts[1].Date);
+const formattedDate3 = formatDate(weather.DailyForecasts[2].Date);
+const formattedDate4 = formatDate(weather.DailyForecasts[3].Date);
+const formattedDate5 = formatDate(weather.DailyForecasts[4].Date);
+
+
+//Getting the day temperature
+const dayTemp1 = weather.DailyForecasts[0].Temperature.Maximum.Value;
+const dayTemp2 = weather.DailyForecasts[1].Temperature.Maximum.Value;
+const dayTemp3 = weather.DailyForecasts[2].Temperature.Maximum.Value;
+const dayTemp4 = weather.DailyForecasts[3].Temperature.Maximum.Value;
+const dayTemp5 = weather.DailyForecasts[4].Temperature.Maximum.Value;
 
 //Rewritten and updated HTML
-weatherSummary.innerHTML = `
+daySummary.innerHTML = `
+<li>
+<p>${formattedDate1}</p>
+<p>${dayTemp1}</p>
+<img src="${iconSrc1}">
+</li>
+<li>
+<p>${formattedDate2}</p>
+<p>${dayTemp2}</p>
+<img src="${iconSrc2}">
+</li>
+<li>
+<p>${formattedDate3}</p>
+<p>${dayTemp3}</p>
+<img src="${iconSrc3}">
+</li>
+<li>
+<p>${formattedDate4}</p>
+<p>${dayTemp4}</p>
+<img src="${iconSrc4}">
+</li>
+<li>
+<p>${formattedDate5}</p>
+<p>${dayTemp5}</p>
+<img src="${iconSrc5}">
+</li>
 <h1 id="city_heading">${cityDetails.EnglishName}</h1>
-<p id="temperature">${weather.Temperature.Imperial.Value + '°F/' + weather.Temperature.Metric.Value + '°C'}</p>
-<p id="forecast_headline">${weather.WeatherText}</p>
-<img id="icon" src="${iconSrc}" alt="">
+<p id="temperature">${weather.DailyForecasts[0].Temperature.Maximum.Value + '°F/'}</p>
+<p id="forecast_headline">${weather.DailyForecasts[0].Day.IconPhrase}</p>
+<img id="icon" src="${iconSrc3}" alt="">;
 `;
 
 //Check if its day or night
@@ -30,8 +83,8 @@ let changeBackgroundColor = null;
 weatherPic.style.background = changeBackgroundColor;
 
 //Remove displayNone class if anything appears
-if (weatherSummary.classList.contains('displayNone')) {
-    weatherSummary.classList.remove('displayNone')
+if (daySummary.classList.contains('displayNone')) {
+    daySummary.classList.remove('displayNone')
 };
 
 };
