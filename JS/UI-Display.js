af506e498066eda529d723a9b9e73ca25b1f2fda
Neto3999 <netowashere@gmail.com>
2023-07-27 17:51:09 -0700

    UI looks presentable-ish
    The UI looks somewhat there, the vision is to have the current weather on top with the 7 days of the week branch underneath.

--- JS/UI-Display.js
+++ JS/UI-Display.js
@@ -9,15 +9,6 @@ const apiData = (data) => {
 
     const { cityDetails, weather } = data;
 
-
-//Weather icon
-const iconSrc1 = `SamplePng/${weather.DailyForecasts[0].Day.Icon}.png`;
-const iconSrc2 = `SamplePng/${weather.DailyForecasts[1].Day.Icon}.png`;
-const iconSrc3 = `SamplePng/${weather.DailyForecasts[2].Day.Icon}.png`;
-const iconSrc4 = `SamplePng/${weather.DailyForecasts[3].Day.Icon}.png`;
-const iconSrc5 = `SamplePng/${weather.DailyForecasts[4].Day.Icon}.png`;
-
-
 //Formatting the date
 function formatDate(dateString) {
     const dateObject = new Date(dateString);
@@ -33,7 +24,6 @@ const formattedDate3 = formatDate(weather.DailyForecasts[2].Date);
 const formattedDate4 = formatDate(weather.DailyForecasts[3].Date);
 const formattedDate5 = formatDate(weather.DailyForecasts[4].Date);
 
-
 //Getting the day temperature
 const dayTemp1 = weather.DailyForecasts[0].Temperature.Maximum.Value + "° / " + 
 weather.DailyForecasts[0].Temperature.Minimum.Value + "°";
@@ -46,49 +36,56 @@ weather.DailyForecasts[3].Temperature.Minimum.Value + "°";
 const dayTemp5 = weather.DailyForecasts[4].Temperature.Maximum.Value + "° / " + 
 weather.DailyForecasts[4].Temperature.Minimum.Value + "°";
 
-//Forecast
+//Getting the Forecast
 const forecast1 = weather.DailyForecasts[0].Day.IconPhrase;
 const forecast2 = weather.DailyForecasts[1].Day.IconPhrase;
 const forecast3 = weather.DailyForecasts[2].Day.IconPhrase;
 const forecast4 = weather.DailyForecasts[3].Day.IconPhrase;
 const forecast5 = weather.DailyForecasts[4].Day.IconPhrase;
 
+//Weather icon
+const iconSrc1 = `SamplePng/${weather.DailyForecasts[0].Day.Icon}.png`;
+const iconSrc2 = `SamplePng/${weather.DailyForecasts[1].Day.Icon}.png`;
+const iconSrc3 = `SamplePng/${weather.DailyForecasts[2].Day.Icon}.png`;
+const iconSrc4 = `SamplePng/${weather.DailyForecasts[3].Day.Icon}.png`;
+const iconSrc5 = `SamplePng/${weather.DailyForecasts[4].Day.Icon}.png`;
+
 //Rewritten and updated HTML
 cityName.innerHTML = `${cityDetails.EnglishName}`;
 daySummary.innerHTML = `
-<li>
-<p>${formattedDate1}</p>
-<p>${dayTemp1}</p>
-<p>${forecast1}</p>
-<img src="${iconSrc1}">
-</li>
-<li>
-<p>${formattedDate2}</p>
-<p>${dayTemp2}</p>
-<p>${forecast2}</p>
-<img src="${iconSrc2}">
-</li>
-<li>
-<p>${formattedDate3}</p>
-<p>${dayTemp3}</p>
-<p>${forecast3}</p>
-<img src="${iconSrc3}">
-</li>
-<li>
-<p>${formattedDate4}</p>
-<p>${dayTemp4}</p>
-<p>${forecast4}</p>
-<img src="${iconSrc4}">
-</li>
-<li>
-<p>${formattedDate5}</p>
-<p>${dayTemp5}</p>
-<p>${forecast5}</p>
-<img src="${iconSrc5}">
-</li>
+<div id="day1">
+<p class="date">${formattedDate1}</p>
+<p class="temperature">${dayTemp1}</p>
+<p class="forecastHeadline">${forecast1}</p>
+<img class="icon" src="${iconSrc1}">
+</div>
+<div id="day2">
+<p class="date">${formattedDate2}</p>
+<p class="temperature">${dayTemp2}</p>
+<p class="forecastHeadline">${forecast2}</p>
+<img class="icon" src="${iconSrc2}">
+</div>
+<div id="day3">
+<p class="date">${formattedDate3}</p>
+<p class="temperature">${dayTemp3}</p>
+<p class="forecastHeadline">${forecast3}</p>
+<img class="icon" src="${iconSrc3}">
+</div>
+<div id="day4">
+<p class="date">${formattedDate4}</p>
+<p class="temperature">${dayTemp4}</p>
+<p class="forecastHeadline">${forecast4}</p>
+<img class="icon" src="${iconSrc4}">
+</div>
+<div id="day5">
+<p class="date">${formattedDate5}</p>
+<p class="temperature">${dayTemp5}</p>
+<p class="forecastHeadline">${forecast5}</p>
+<img class="icon" src="${iconSrc5}">
+</div>
 `;
 
-//Check if its day or night
+//Check if its day or night, doesnt function for 7 days of the week
 let changeBackgroundColor = null;
     if (weather.IsDayTime) {
         changeBackgroundColor = 'linear-gradient(#87ceeb, white) no-repeat';
@@ -97,9 +94,7 @@ let changeBackgroundColor = null;
     };
 weatherPic.style.background = changeBackgroundColor;
 
-//Remove displayNone class if anything appears
-if (daySummary.classList.contains("displayNone")) {
-    daySummary.classList.remove("displayNone")
-};
+//Change display to grid if anything appears
+daySummary.style.display = "grid";
 
 };
