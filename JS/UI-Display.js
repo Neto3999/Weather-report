9135ca05372fe226880e73df3baf23256f2ab38d
Neto3999 <netowashere@gmail.com>
2023-07-29 01:17:54 -0700

    Added support to both API's
    both work in conjunction, fixed background syncing if its daytime, need to shorten UI displays amount of lines if possible

--- JS/UI-Display.js
+++ JS/UI-Display.js
@@ -2,12 +2,13 @@ const daySummary = document.getElementById("day_forecast");
 const weatherPic = document.querySelector("body");
 const icon = document.getElementById("icon");
 const cityName = document.getElementById("city_heading");
+const todayForcast = document.getElementById("weather_summary");
 
 const apiData = (data) => {
 
     console.log(data);
 
-    const { cityDetails, weather } = data;
+    const { cityDetails, weather, currentWeather } = data;
 
 //Formatting the date//
 function formatDate(dateString) {
@@ -25,6 +26,7 @@ const formattedDate4 = formatDate(weather.DailyForecasts[3].Date);
 const formattedDate5 = formatDate(weather.DailyForecasts[4].Date);
 
 //Getting the day temperature//
+const dayTemp0 = currentWeather.Temperature.Imperial.Value + "°F"
 const dayTemp1 = weather.DailyForecasts[0].Temperature.Maximum.Value + "° / " + 
 weather.DailyForecasts[0].Temperature.Minimum.Value + "°";
 const dayTemp2 = weather.DailyForecasts[1].Temperature.Maximum.Value + "° / " + 
@@ -37,6 +39,7 @@ const dayTemp5 = weather.DailyForecasts[4].Temperature.Maximum.Value + "° / " +
 weather.DailyForecasts[4].Temperature.Minimum.Value + "°";
 
 //Getting the Forecast//
+const forecast0 = currentWeather.WeatherText;
 const forecast1 = weather.DailyForecasts[0].Day.IconPhrase;
 const forecast2 = weather.DailyForecasts[1].Day.IconPhrase;
 const forecast3 = weather.DailyForecasts[2].Day.IconPhrase;
@@ -44,6 +47,7 @@ const forecast4 = weather.DailyForecasts[3].Day.IconPhrase;
 const forecast5 = weather.DailyForecasts[4].Day.IconPhrase;
 
 //Weather icon//
+const iconSrc0 = `SamplePng/${currentWeather.WeatherIcon}.png`;
 const iconSrc1 = `SamplePng/${weather.DailyForecasts[0].Day.Icon}.png`;
 const iconSrc2 = `SamplePng/${weather.DailyForecasts[1].Day.Icon}.png`;
 const iconSrc3 = `SamplePng/${weather.DailyForecasts[2].Day.Icon}.png`;
@@ -52,6 +56,12 @@ const iconSrc5 = `SamplePng/${weather.DailyForecasts[4].Day.Icon}.png`;
 
 //Rewritten and updated HTML//
 cityName.innerHTML = `${cityDetails.EnglishName}`;
+todayForcast.innerHTML = `
+<p class="temperature">Currently: ${dayTemp0}</p>
+<p class="forecastHeadline">${forecast0}</p>
+<img class="icon" src="${iconSrc0}">
+`;
+
 daySummary.innerHTML = `
 <div id="day1">
 <p class="date">${formattedDate1}</p>
@@ -87,7 +97,7 @@ daySummary.innerHTML = `
 
 //Check if its day or night, doesnt function for 7 days of the week//
 let changeBackgroundColor = null;
-    if (weather.IsDayTime) {
+    if (currentWeather.IsDayTime) {
         changeBackgroundColor = 'linear-gradient(#87ceeb, white) no-repeat';
     } else {
         changeBackgroundColor = 'linear-gradient(#2b1055, #7597de) no-repeat';
@@ -96,5 +106,6 @@ weatherPic.style.background = changeBackgroundColor;
 
 //Change display to grid if anything appears//
 daySummary.style.display = "grid";
+todayForcast.style.display = "contents";
 
 };
